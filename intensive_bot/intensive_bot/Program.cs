ea3f25324da456b919a0a96283c3d3d8f3e9c706
ODIN74 <fantompus@gmail.com>
2018-03-18 20:40:49 +0500

    First step

--- intensive_bot/intensive_bot/Program.cs
+++ intensive_bot/intensive_bot/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+using System.Net;
 
 namespace intensive_bot
 {
@@ -10,6 +13,27 @@ namespace intensive_bot
     {
         static void Main(string[] args)
         {
+            int update_id = 0;
+            string messageFromId = "";
+            string messageText = "";
+            string first_name = "";
+            string token = "594904432:AAFPNbUe2fNeSs9Z7CJCMnwbdx-4d74DjEQ";
+
+            WebClient webClient = new WebClient();
+
+            string startUrl = $"https://api.telegram.org/bot{token}";
+
+            while (true)
+            {
+                startUrl = $"{startUrl}/getUpdte?offset={update_id = 1}";
+                string response = webClient.DownloadString(url);
+
+                var array =  JObject.Parse(response)["result"].ToArray();
+
+            
+
+            }
+
         }
     }
 }
